ec75c0139b48bfc2e06011e271aee5009e5d771e
dirkarnez <smalldirkalex@gmail.com>
2024-02-26 21:54:38 +0800

    - working

--- local-compile-circle.sh
+++ local-compile-circle.sh
@@ -4,6 +4,9 @@ export PATH="$PATH:$USERPROFILE/Downloads/xpack-aarch64-none-elf-gcc-13.2.1-1.1-
 # export PATH="$PATH:$USERPROFILE/Downloads/gcc-arm-none-eabi-10-2020-q4-major/bin"
 export MAKE="$USERPROFILE/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/bin/mingw32-make.exe"
 
-cd src && \
-$MAKE $1 $2 && \
+
+cd $USERPROFILE/Downloads/circle && \
+./configure --raspberrypi 4 --realtime --prefix aarch64-none-elf- --multicore --c++17  && \
+./makeall clean && \
+./makeall && \
 read -p "done"
